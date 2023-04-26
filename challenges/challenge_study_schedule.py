3b41bb36381d831f718caa8570ac4d5150a72223
Nayara Vasconcelos <nayaravasconcelos.dev@gmail.com>
2023-04-26 18:26:03 -0300

    feat: adiciona a funcao study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,22 @@
+# Problema:
+# Descobrir o horário que tem a maior quantidade de pessoas acessando a plataforma
+# Dados: Tuple(horário de entrada, horario de saída) -> permanece_period
+# Estratégia: Força bruta
+
+# verificar se o horário de entrada é menor ou igual ao target_time
+# ou se o horário de saída é maior ou igual ao target_time
+# se verdadeiro, counter += 1
+# retornar counter
+
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None:
+        return None
+    
+    counter = 0
+    for login_time, logout_time in permanence_period:
+        if (not isinstance(login_time, int)) or (not isinstance(logout_time, int)):
+            return None
+
+        if login_time <= target_time <= logout_time: counter += 1
+
+    return counter
