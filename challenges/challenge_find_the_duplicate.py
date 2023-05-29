d975878a1880632a7be64ebf1e8c99ec43a89882
Nayara Vasconcelos <nayaravasconcelos.dev@gmail.com>
2023-05-29 18:41:51 -0300

    feat: adiciona a funcao find_duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,23 @@
+def check_input(input):
+    valid_input = (
+        input is not None and isinstance(input, list) and len(input) > 1
+    )
+    if not valid_input:
+        return False
+
+    for element in input:  # O(n)
+        valid_value = isinstance(element, int) and (element > 0)
+        if not valid_value:
+            return False
+
+    return True
+
+
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if check_input(nums):
+        ordered_nums = sorted(nums)  # O(n log n)
+        for i in range(1, len(ordered_nums)):  # O(n)
+            if ordered_nums[i - 1] == ordered_nums[i]:
+                return ordered_nums[i - 1]
+
+    return False
