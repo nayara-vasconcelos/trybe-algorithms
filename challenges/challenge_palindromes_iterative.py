af1cb12002b01803d079066ad8d4114d9e7f32d3
Nayara Vasconcelos <nayaravasconcelos.dev@gmail.com>
2023-05-29 19:00:03 -0300

    feat: adiciona a funcao is_palindrome_iterative

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,11 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(word) == 0 or not isinstance(word, str):
+        return False
+
+    char_left, char_right = 0, -1
+    mid_position = len(word) // 2
+    for i in range(char_left, mid_position):  # O(n)
+        if word[char_left + i] != word[char_right - i]:
+            return False
+
+    return True
